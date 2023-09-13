a275ece371d60c110e7a0a15fa54f7a4bbf72513
sdevmarc <suanieeee@yahoo.com>
2023-09-13 19:11:15 +0800

    New UI

---
+++ main/php/dashboard.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIS | Dashboard</title>
+    <link rel="stylesheet" href="/dbfiles/ias/sisv2/main/css/dashboard.css">
+</head>
+
+<body>
+    <section>
+        <div class="wrapper">
+            <div class="leftbar">
+
+                <div class="dashboard">
+                    <a href="">DASHBOARD</a>
+                </div>
+                <div class="dean">
+                    <div class="title">
+                        DEAN
+                    </div>
+                    <div class="navDean">
+                        <a href="">SEARCH</a>
+                        <a href="">ADD ADMISSION</a>
+                        <a href="">UPDATE ADMISSION</a>
+                        <a href="">DELETE ADMISSION</a>
+                        <a href="">AUDIT LOG</a>
+                    </div>
+                </div>
+                <div class="attendance">
+                    <div class="title">
+                        ATTENDANCE
+                    </div>
+                    <div class="navAttendance">
+                        <a href="">SEARCH</a>
+                        <a href="">ADD ADMISSION</a>
+                        <a href="">UPDATE ADMISSION</a>
+                        <a href="">DELETE ADMISSION</a>
+                        <a href="">AUDIT LOG</a>
+                    </div>
+                </div>
+                <div class="settings">
+                    <div class="title">
+                        SETTINGS
+                    </div>
+                    <div class="navSettings">
+                        <a href="">AUDIT LOG</a>
+                        <a href="">MANAGE USER</a>
+                        <a href="">MANAGE UI</a>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="header">
+                    <div class="hamburger">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="title">
+                        OVERVIEW
+                    </div>
+                </div>
+                <div class="contents">
+                    <div class="analysis">
+                        <div class="title">
+                            DAILY STUDENT ATTENDANCE ANALYSIS
+                        </div>
+
+                        <div class="analysis-content">
+                            <div class="analysis-title">
+                                ADMISSIONS
+                            </div>
+                            <div class="analysis-progress">
+                                test
+                            </div>
+                        </div>
+                    </div>
+                    <div class="other-analysis">
+                        <div class="late">
+                            <div class="title">
+                                LATE ADMISSIONS
+                            </div>
+                            <div class="progress">
+                                test
+                            </div>
+                        </div>
+                        <div class="absent">
+                            <div class="title">
+                                ABSENT ADMISSIONS
+                            </div>
+                            <div class="progress">
+                                test
+                            </div>
+                        </div>
+                        <div class="excused">
+                            <div class="title">
+                                EXCUSED ADMISSIONS
+                            </div>
+                            <div class="progress">
+                                test
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="rightbar">
+
+            </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
