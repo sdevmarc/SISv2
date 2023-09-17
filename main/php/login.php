5efdd20af33449bcc7ad25de0f95ab1367db712c
Marc Edison <suanieeee@yahoo.com>
2023-09-17 18:43:34 +0800

    login design goods

---
+++ main/php/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIS | Login</title>
+    <link rel="stylesheet" href="/dbfiles/ias/sisv2/main/css/login.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-logo">
+            <div class="first">
+                Saint Mary's University
+            </div>
+            <div class="second">
+                Bayombong, Nueva Vizcaya
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="login-form">
+                <form action="" method="post">
+                    <div class="details">
+                        <p>Unlock Your World,<br>One Login at a Time.</p>
+                        <p>Welcome back, <br>Please login to your account</p>
+                    </div>
+                    <div class="inputs">
+                        <div class="box input-box">
+                            <input name="username" placeholder="Username" type="text" required>
+                        </div>
+                        <div class="box input-box">
+                            <input name="password" placeholder="Password" type="password" required>
+                        </div>
+                        <div class="buttons">
+                            <button id="login" name="login">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="container-logo">
+                <div class="details">
+                    <p>Nurturing <br> Minds, <br>
+                    </p>
+                    <p> Shaping <br> Futures.
+                    </p>
+                </div>
+                <!-- <img src="https://pbs.twimg.com/media/FteWD0saAAMIVVZ.jpg" alt=""> -->
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
