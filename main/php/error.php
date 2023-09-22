c5b024d2f7dfcfe77f99182c698a46af27b4160c
sdevmarc <suanieeee@yahoo.com>
2023-09-22 15:00:46 +0800

    Made an error page

---
+++ main/php/error.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <link rel="stylesheet" href="/dbfiles/ias/sisv2/main/css/error.css">
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAeBJREFUSEvFli1vFUEUhp83GBwSCQgkxYApn6KqrayhAVLZVOEKv4AGhSG1NOmHaOpaHAmEJggwUFnRIpE4DLzkJBcyd+9OZ3fZm3vMZHdm3uecnDlzRkzINCEuncG2ZwBLetvF+U5g29PA4QB4S9LHtvCu4E/AjQHsUNKdsYNtzwH7FdCcpDdt4K0jtv0VuFaBHEmaGhvY9iKwlQEsStppCm8cse1zwDFwJSN+AlyV9KsJvA14BXhVEF2RtN4b2PZ54BS4WBD9DlyW9LMEbxSx7afA80TsC/Bk8P0SuJ7MrUp68d9g2xeAb0CMf+29pPvxYfsdcC+Z+wFckhRj1ooR214DVisKZ4Fj6ZqkZ53BtiOnkdvIcWolcOQ4ch05r7UzI7YdJ3S5ZmcJHFvWJUUltAPbjnqNuo36rVoTcNRz1HXU94hlI7a9DTzIOByH7fVgbikOU2bdlqSHjcG24y6OO7kPm5J0VBWqjdj2ATDbBxU4kDRfBNu+DXwoQJvkOJW4Kelz+mMkYttpk8/x24JHHgtD4EyTr4M3PVzp3qHHQhVc1+R7SjVDj4V/YNsLwG5flIzOgqS9mEvBj4GNMYMfSdqsgsOJu6kzPTvxO6pFkofAPUOKcsW2WFTouGBi4D8iZ6wfzyfbFgAAAABJRU5ErkJggg==">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <section>
+        <div class="container">
+            <div class="error">
+                <div class="error-image">
+                    <img src="https://pbs.twimg.com/media/FteWD0saAAMIVVZ.jpg" alt="">
+                </div>
+                <div class="error-msg">
+                    <div class="title">
+                        <h1>404: Page Not Found</h1>
+                    </div>
+                    <div class="message">
+                        <p>We're sorry, but the page you're looking for cannot be found. It might have been moved, deleted, or simply doesn't exist. <br> Please check the URL for any typos or errors, and make sure you're using the correct link.</p>
+                    </div>
+                    <div class="direction">
+                        <a href="/dbfiles/ias/sisv2/main/php/dashboard.php">Go to Homepage<i class='bx bx-chevron-right'></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="contents">
+            <div class="title">
+                Contact Us
+            </div>
+            <div class="message">
+                If you believe this is a technical issue or if you need further assistance, please don't hesitate to <a href="">contact our support team.</a> We're here to help!
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
