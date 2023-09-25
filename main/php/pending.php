9ea4342436d94382eebb675f81b5d78ff7f49ef9
Marc Edison <suanieeee@yahoo.com>
2023-09-25 22:33:10 +0800

    added a pending page

---
+++ main/php/pending.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pending</title>
+    <link rel="stylesheet" href="/dbfiles/ias/sisv2/main/css/pending.css">
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAARBJREFUSEu9leERgjAMhd/bRDfBScRJkElkE91ENokXr/Xa0paUA/nDD5r3JWleIA5+eLA+mgAiIpoQSXOc+aAK7wYQkQeAkeQctrAEEJETgI7klLZ8UYET7wGo+CWE5ABOXBPqANxJjiEkB9BsngD0HUFSQCI+kzyvVuB6HUFyge6cJqKZZ8W/A1EaU5edCuhdLHrrAAOAvpRAFbCXP5rGdAv0B/AXGE2A0VC12P8BtpRvianegU5S6maLaNVo/mNgolfqzuCMOl5HNXL8KsDiUOeDd87xllXhd0vk0MKqyK4VD8ntIi1bAQv7V5adh0wkb5YKrrm+r6zrIRVvXhW7/XAqC/DYX2arB5pbtAXwAawMoBkg19g9AAAAAElFTkSuQmCC">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <section>
+        <div class="container">
+            <div class="pending">
+                <div class="pending-image">
+                    <img src="https://pbs.twimg.com/media/FteWD0saAAMIVVZ.jpg" alt="">
+                </div>
+                <div class="pending-msg">
+                    <div class="title">
+                        <h1>Pending Account Activation</h1>
+                    </div>
+                    <div class="message">
+                        <p>Thank you for registering with our website! Your account is almost ready,<br> but it requires final approval from our administrators before it becomes fully active.</p>
+                    </div>
+                    <div class="direction">
+                        <a href="/dbfiles/ias/sisv2/main/php/login.php">Go back<i class='bx bx-chevron-right'></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="contents">
+            <div class="title">
+                Contact Us
+            </div>
+            <div class="message">
+                If you have any urgent questions or concerns, please feel free to reach out to our support team at <a href="">contact our support team.</a> We're here to assist you!
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
