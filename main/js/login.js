76f13c4b35338056cd1bbb46e11589934e77d784
Marc Edison <suanieeee@yahoo.com>
2023-09-25 22:30:51 +0800

    Login show and hide pass done

--- main/js/login.js
+++ main/js/login.js
@@ -1,8 +1,36 @@
+let input = document.getElementById('password');
+
+input.addEventListener('focus', () => {
+    document.querySelector('.show-pas').style.display = 'block';
+});
+
 document.getElementById('password').addEventListener('keyup', (e) => {
     if (e.getModifierState('CapsLock')) {
         document.querySelector('.capsState').style.display = 'block';
     }
     else {
         document.querySelector('.capsState').style.display = 'none';
+    }
+});
+
+document.querySelector('.bx-show').addEventListener('click', () => {
+    document.querySelector('.bx-hide').style.display = 'block';
+    document.querySelector('.bx-show').style.display = 'none';
+    let input = document.getElementById('password');
+    if (input.type === 'password') {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+});
+
+document.querySelector('.bx-hide').addEventListener('click', () => {
+    document.querySelector('.bx-show').style.display = 'block';
+    document.querySelector('.bx-hide').style.display = 'none';
+    let input = document.getElementById('password');
+    if (input.type === 'text') {
+        input.type = 'password';
+    } else {
+        input.type = 'text';
     }
 });
