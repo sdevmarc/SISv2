9f10bd7a5087abe4e4d3c8c8733671c6575567bd
sdevmarc <suanieeee@yahoo.com>
2023-09-28 17:08:44 +0800

    Changed time value

--- main/php/dashboard.php
+++ main/php/dashboard.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['username'])) {
     header('Location: logout.php');
     exit();
 } else {
-    if((time() - $_SESSION['last_login_timestamp']) > 60)  {
+    if((time() - $_SESSION['last_login_timestamp']) > 6)  {
         header('Location: logout.php');
         ob_end_flush();
         exit();
